fae4415bf83e0f7a86ee9d3d0a01d3913e39cf2e
huss0249 <ahmedsabry@outlook.com>
2024-02-15 17:49:10 -0500

    going well

--- script.js
+++ script.js
@@ -5,17 +5,102 @@ console.log(csv_source.length)
 // const $myData = document.querySelector(".myData")
 const $myGrid = document.querySelector("#myGrid")
 
+$myGrid.classList.add("d-flex", "flex-column");
+
 //================================================================================
-csv_source.forEach(el => {
-//   console.log(el)
+// csv_source.forEach(el => {
+// //   console.log(el)
+//   for (let [key, value] of Object.entries(el)) {
+//     key === 'ID' ? console.log(`${key}: ${value}`) : ''
+//     console.log(`${key}: ${value}`)
+//   }
+//   console.log(' ')
+// })
+
+
+const fragment = new DocumentFragment();
+
+csv_source.forEach((el) => {
+  //   console.log(el)
   for (let [key, value] of Object.entries(el)) {
-    key === 'ID' ? console.log(`${key}: ${value}`) : ''
-    console.log(`${key}: ${value}`)
+    // key === 'ID' ? console.log(`${key}: ${value}`) : ''
+    // console.log(`${key}: ${value}`)
+
+    if (key === "ID") {
+      const $btn = document.createElement("button");
+      $btn.id = value;
+      $btn.type = "button";
+      $btn.classList.add("m-3", "p-2", "bg-success");
+
+      $btn.textContent = el["Description"];
+      // ? console.log(`${key}: ${value}`) : ''
+      el["Next"] ? ($btn.dataset.next = el["Next"]) : "";
+      el["Type"] ? ($btn.dataset.type = el["Type"]) : "";
+      el["Function"] ? ($btn.dataset.function = el["Function"]) : "";
+      el["Phase"] ? ($btn.dataset.phase = el["Phase"]) : "";
+      el["Connector"] ? ($btn.dataset.connector = el["Connector"]) : "";
+      el["Link"] ? ($btn.dataset.link = el["Link"]) : "";
+      fragment.append($btn);
+    }
   }
-  console.log(' ')
-})
+  $myGrid.append(fragment);
+
+  // console.log(el.['Next']);
+
+  // let fromm = `#${el["ID"]}`;
+  // let too = `#${el["Next"]}`;
+
+  // console.log(fromm, too)
+
+  // let m = new LeaderLine(
+  //   // document.querySelector(`#${fromm}`),
+  //   // document.querySelector(`#${too}`)
+  //   document.querySelector(fromm),
+  //   document.querySelector(too)
+  // );
+});
 
 
+// csv_source.forEach((el, index) => {
+  
+//   console.log(csv_source[index]['ID'])
+//   let fromm = `#${csv_source[index]["ID"]}`;
+//   let too = `#${csv_source[index]["Next"]}`;
+//   console.log(fromm, too)
+  
+//   // let m = new LeaderLine(
+//     //   // document.querySelector(`#${fromm}`),
+//     //   // document.querySelector(`#${too}`)
+//     //   document.querySelector(fromm),
+//     //   document.querySelector(too)
+//     // );
+//     fromm != undefined && too != undefined ? window['m'+index] = new LeaderLine(
+//   // let [index] = new LeaderLine(
+//     // document.querySelector(`#${fromm}`),
+//     // document.querySelector(`#${too}`)
+//     document.querySelector(fromm),
+//     document.querySelector(too)
+//   ) : ''
+// });
+
+
+let $btns = document.querySelectorAll('button')
+// console.log($btns[1].dataset.next)
+
+$btns.forEach((el, index) => {
+  
+  // console.log($btns[index])
+  // let fromm = `#${$btns[index]["ID"]}`;
+  // let too = `#${$btns[index]["Next"]}`;
+  let fromm = `#${$btns[index].id}`;
+  let too = `#${$btns[index].dataset.next}`;
+  console.log(fromm, too)
+  
+  // fromm != undefined && too != undefined ? window['m'+index] = new LeaderLine(
+  //   document.querySelector(fromm),
+  //   document.querySelector(too)
+  // ) : ''
+});
 
 /*================================================================================
 ==================================================================================
@@ -44,3 +129,9 @@ let a = new LeaderLine(
   ==================================================================================
   END LEADER-LINE
   ================================================================================*/
+
+  // let mm = new LeaderLine(
+  //   document.querySelector("#P100"),
+  //   document.querySelector("#P200")
+  // );
+  
