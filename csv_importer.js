db0fce22cb31dfca1a4d742d2af01192b6321579
huss0249 <ahmedsabry@outlook.com>
2024-02-23 22:54:19 -0500

    oo

--- csv_importer.js
+++ csv_importer.js
@@ -7,14 +7,17 @@ const $myData = document.querySelector(".myData")
 function CSVstring_to_Array(data, delimiter = ",") {
   // This variable will collect all the titles from the data variable ["Name", "Roll Number"]
   const titles = data.slice(0, data.indexOf("\n")).split(delimiter);
+  console.log('titles=> ', titles)
   
   // This variable will store the values from the data [ 'Rohan,01', 'Aryan,02' ]
   const titleValues = data.slice(data.indexOf("\n") + 1).split("\n");
+  console.log('titleValues=> ', titleValues)
   
   // Map function will iterate over all values of title values array and append each object at the end of the array
   const ansArray = titleValues.map(function (v) {
     // Values variable will store individual title values [ 'Rohan', '01' ]
     const values = v.split(delimiter);
+    console.log('values=> ', values)
     
     // storeKeyValue variable will store object containing each title with their respective values i.e { Name: 'Rohan', 'Roll Number': '01' }
     const storeKeyValue = titles.reduce(function (obj, title, index) {
